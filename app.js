97a779ccc632b0fa3afc4c6fa8a7b77884c6c01c
revanth-1807 <revanth1807r@gmail.com>
2025-09-03 09:48:16 +0530

    app.js

--- app.js
+++ app.js
@@ -7,14 +7,9 @@ const app = express();
 // Middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-
-// DB connection (MongoDB local or Atlas)
-// mongoose.connect('mongodb://127.0.0.1:27017/portfolioDB', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => console.log("MongoDB Connected"))
-//   .catch(err => console.log(err));
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Routes
 const pageRoutes = require('./routes/pages');
